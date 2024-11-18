766f36414532975cabe4a7709dbc819d40e54833
09NINE90 <razumov.aleksei101@yandex.ru>
2024-11-18 23:56:30 +0500

    first commit

---
+++ src/main/java/ru/platform/BoosterPlatformApplication.java
@@ -0,0 +1,13 @@
+package ru.platform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoosterPlatformApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BoosterPlatformApplication.class, args);
+    }
+
+}
