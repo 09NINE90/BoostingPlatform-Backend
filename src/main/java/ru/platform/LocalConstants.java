68aab508a0d8b4d34e45974a24fd100c42b850d6
09NINE90 <razumov.aleksei101@yandex.ru>
2024-11-26 22:10:11 +0500

    #back Поменял роль по умолчанию, добавил класс с константами

---
+++ src/main/java/ru/platform/LocalConstants.java
@@ -0,0 +1,10 @@
+package ru.platform;
+
+public class LocalConstants {
+
+    public static class Variables{
+        public static final int DEFAULT_PAGE_SIZE = 20;
+        public static final int DEFAULT_PAGE_NUMBER = 0;
+
+    }
+}
