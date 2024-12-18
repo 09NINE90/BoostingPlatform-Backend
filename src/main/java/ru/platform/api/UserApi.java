1f19c5fcb877ba885f23a16ce8749c323d5eb593
razum0ff <razumov.aleksei101@yandex.ru>
2024-12-18 12:23:58 +0500

    fix

--- src/main/java/ru/platform/api/UserApi.java
+++ src/main/java/ru/platform/api/UserApi.java
@@ -1,12 +1,8 @@
 package ru.platform.api;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.ModelAndView;
-import ru.platform.dto.CustomUserDetails;
 import ru.platform.entity.UserEntity;
-import ru.platform.entity.enums.ERoles;
 import ru.platform.service.IUserService;
 
 import java.util.List;
@@ -18,31 +14,9 @@ public class UserApi {
 
     private final IUserService service;
 
-    @GetMapping("/mainPage")
-    public ModelAndView getMainPage(Authentication authentication){
-        ModelAndView modelAndView = new ModelAndView();
-        CustomUserDetails userDetails = (CustomUserDetails) authentication.getPrincipal();
-        if (userDetails.getRole().equals(ERoles.ADMIN.getTitle())){
-            modelAndView.setViewName("admin-services");
-        }
-        else {
-            modelAndView.setViewName("main-page");
-        }
-        return modelAndView;
-    }
-
-    @GetMapping("/getSignupForm")
-    public ModelAndView getSignupForm(){
-        ModelAndView modelAndView = new ModelAndView();
-        modelAndView.setViewName("signup-form");
-        return modelAndView;
-    }
-
     @GetMapping("/getAllUsers")
     public List<UserEntity> getAllUsers(){
         return service.getAllUsers();
     }
-
-
 
 }
