db1bc8f5a4e50bec79d262e1a0d405fec53ab5d9
09NINE90 <razumov.aleksei101@yandex.ru>
2025-02-11 23:07:15 +0500

    refactoring

--- src/main/java/ru/platform/config/SecurityConfig.java
+++ src/main/java/ru/platform/config/SecurityConfig.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -20,6 +21,7 @@ import ru.platform.config.auth.JwtAuthenticationFilter;
 import ru.platform.service.impl.SecurityService;
 
 @Configuration
+@EnableMethodSecurity
 public class SecurityConfig {
 
     @Bean
