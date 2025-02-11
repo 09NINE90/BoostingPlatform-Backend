3c2a6269249027eeb9e277964da4d9ccd2fb2573
09NINE90 <razumov.aleksei101@yandex.ru>
2025-02-11 23:14:29 +0500

    refactoring AuthApi

--- src/main/java/ru/platform/api/AuthApi.java
+++ src/main/java/ru/platform/api/AuthApi.java
@@ -3,7 +3,6 @@ package ru.platform.api;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.AuthenticationException;
