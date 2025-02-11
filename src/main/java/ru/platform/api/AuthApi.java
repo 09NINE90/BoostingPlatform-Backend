9aa4d455d5f1afd4d37fd950ad2ef5d9d68b1604
09NINE90 <razumov.aleksei101@yandex.ru>
2025-02-11 23:14:01 +0500

    refactoring AuthApi

--- src/main/java/ru/platform/api/AuthApi.java
+++ src/main/java/ru/platform/api/AuthApi.java
@@ -2,6 +2,7 @@ package ru.platform.api;
 
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -16,14 +17,12 @@ import ru.platform.service.IUserService;
 import java.util.Map;
 
 @RestController
+@RequiredArgsConstructor
 @RequestMapping("/api/auth")
 public class AuthApi {
 
-    @Autowired
-    private IUserService userService;
-
-    @Autowired
-    private IAuthService authService;
+    private final IUserService userService;
+    private final IAuthService authService;
 
     @PostMapping("/signUp")
     public ResponseEntity<?> userSave(@RequestBody UserDTO user){
