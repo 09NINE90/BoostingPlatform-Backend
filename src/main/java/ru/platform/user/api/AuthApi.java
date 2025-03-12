0bbda09f8fe03513b82db8ed71de9d66c34cba26
09NINE90 <razumov.aleksei101@yandex.ru>
2025-03-12 15:19:37 +0500

    эедпоинт для подтверждения почты

--- src/main/java/ru/platform/user/api/AuthApi.java
+++ src/main/java/ru/platform/user/api/AuthApi.java
@@ -3,9 +3,11 @@ package ru.platform.user.api;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import ru.platform.user.dto.request.ConfirmationEmailRqDto;
 import ru.platform.user.dto.request.SignupUserRqDto;
 import ru.platform.user.dto.request.LoginUserRqDto;
-import ru.platform.user.dto.response.AuthResponse;
+import ru.platform.user.dto.response.AuthRsDto;
+import ru.platform.user.dto.response.SignupRsDto;
 import ru.platform.user.service.IAuthService;
 import ru.platform.user.service.IUserService;
 
@@ -18,14 +20,20 @@ public class AuthApi {
     private final IAuthService authService;
 
     @PostMapping("/signUp")
-    public ResponseEntity<AuthResponse> signUp(@RequestBody SignupUserRqDto user) {
-        AuthResponse result = userService.createUser(user);
+    public ResponseEntity<SignupRsDto> signUp(@RequestBody SignupUserRqDto user) {
+        SignupRsDto result = userService.createUser(user);
+        return ResponseEntity.ok(result);
+    }
+
+    @PostMapping("/confirmSignUp")
+    public ResponseEntity<AuthRsDto> confirmSignUp(@RequestBody ConfirmationEmailRqDto confirmation) {
+        AuthRsDto result = userService.checkConfirmationSignUp(confirmation);
         return ResponseEntity.ok(result);
     }
 
     @PostMapping("/signIn")
-    public ResponseEntity<AuthResponse> signIn(@RequestBody LoginUserRqDto user){
-        AuthResponse result = authService.trySignup(user);
+    public ResponseEntity<AuthRsDto> signIn(@RequestBody LoginUserRqDto user){
+        AuthRsDto result = authService.trySignup(user);
         return ResponseEntity.ok(result);
     }
 
