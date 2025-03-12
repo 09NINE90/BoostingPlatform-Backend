9624da7b8696701097752aeee957cece8e5ec369
09NINE90 <razumov.aleksei101@yandex.ru>
2025-03-12 20:45:07 +0500

    Функционал для восстановления пароля

--- src/main/java/ru/platform/user/service/impl/UserService.java
+++ src/main/java/ru/platform/user/service/impl/UserService.java
@@ -2,12 +2,13 @@ package ru.platform.user.service.impl;
 
 import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import ru.platform.notification.IMailService;
 import ru.platform.user.dto.request.ConfirmationEmailRqDto;
 import ru.platform.user.dto.request.LoginUserRqDto;
-import ru.platform.user.dto.response.SignupRsDto;
+import ru.platform.user.dto.response.ConfirmationRsDto;
 import ru.platform.user.dto.request.SignupUserRqDto;
 import ru.platform.user.dao.UserEntity;
 import ru.platform.user.dao.UserProfileEntity;
@@ -21,9 +22,12 @@ import ru.platform.utils.GenerateSecondIdUtil;
 import java.time.LocalDate;
 import java.util.Random;
 
+import static ru.platform.LocalConstants.Message.*;
+import static ru.platform.notification.MailType.PASSWORD_RECOVERY;
 import static ru.platform.notification.MailType.REGISTRATION;
 import static ru.platform.user.UserRolesType.ROLE_CUSTOMER;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class UserService implements IUserService {
@@ -36,9 +40,9 @@ public class UserService implements IUserService {
     private final IMailService mailService;
 
     @Override
-    public SignupRsDto createUser(SignupUserRqDto user) {
+    public ConfirmationRsDto createUser(SignupUserRqDto user) {
         String confirmationCode = generateConfirmationCode();
-        
+
         UserEntity userEntity = UserEntity.builder()
                 .username(user.getUsername())
                 .password(encoder.encode(user.getPassword()))
@@ -61,7 +65,7 @@ public class UserService implements IUserService {
 
         mailService.sendMail(userEntity, REGISTRATION);
 
-        return new SignupRsDto("Confirmation code sent to email");
+        return new ConfirmationRsDto(CONFIRMATION_CODE_MASSAGE);
     }
 
     private String generateConfirmationCode() {
@@ -82,11 +86,59 @@ public class UserService implements IUserService {
                         confirmation.getPassword()
                 ));
             }
-        }catch (Exception e) {
-            throw new EntityNotFoundException("User not found");
+        } catch (Exception e) {
+            throw new EntityNotFoundException(USER_NOT_FOUND);
         }
 
         return null;
+    }
+
+    @Override
+    public ConfirmationRsDto forgotPassword(ConfirmationEmailRqDto confirmation) {
+        try {
+            UserEntity user = userRepository.findByUsername(confirmation.getEmail());
+            String confirmationCode = generateConfirmationCode();
+
+            user.setConfirmationCode(confirmationCode);
+            userRepository.save(user);
+            mailService.sendMail(user, PASSWORD_RECOVERY);
+
+            return new ConfirmationRsDto(CONFIRMATION_CODE_MASSAGE);
+        } catch (Exception e) {
+            throw new EntityNotFoundException(USER_NOT_FOUND);
+        }
+    }
+
+    @Override
+    public ConfirmationRsDto confirmPasswordRecovery(ConfirmationEmailRqDto confirmation) {
+        try {
+            UserEntity user = userRepository.findByUsername(confirmation.getEmail());
+            String code = confirmation.getConfirmationCode();
+
+            if (user.getConfirmationCode().equals(code)) {
+                return new ConfirmationRsDto(SUCCESS_PASSWORD_RECOVERY);
+            }
+        } catch (Exception e) {
+            throw new EntityNotFoundException(USER_NOT_FOUND);
+        }
+
+        return null;
+    }
+
+    @Override
+    public AuthRsDto changeUserPassword(ConfirmationEmailRqDto confirmation) {
+        try {
+            UserEntity user = userRepository.findByUsername(confirmation.getEmail());
+            user.setPassword(encoder.encode(confirmation.getPassword()));
+            userRepository.save(user);
+
+            return authService.trySignup(new LoginUserRqDto(
+                    confirmation.getEmail(),
+                    confirmation.getPassword()
+            ));
+        } catch (Exception e) {
+            throw new EntityNotFoundException(USER_NOT_FOUND);
+        }
     }
 
 }
