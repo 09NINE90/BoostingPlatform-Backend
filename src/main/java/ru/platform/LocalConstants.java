4a7a4196d4011eff3c3d65d5a97080581ef080f9
09NINE90 <razumov.aleksei101@yandex.ru>
2025-03-20 14:13:16 +0500

    Вынес описание тегов и заглушки для openapi в константы

--- src/main/java/ru/platform/LocalConstants.java
+++ src/main/java/ru/platform/LocalConstants.java
@@ -2,12 +2,31 @@ package ru.platform;
 
 public class LocalConstants {
 
+    public static class Api {
+        public static final String OFFER_TAG_NAME = "Предложения";
+        public static final String OFFER_TAG_DESCRIPTION = "Получение предложений для клиентов";
+        public static final String AUTH_TAG_NAME = "Пользователь";
+        public static final String AUTH_TAG_DESCRIPTION = "Аутентификация, авторизация, подтверждение регистрации и восстеновление пароля";
+        public static final String GAME_TAG_NAME = "Игры";
+        public static final String GAME_TAG_DESCRIPTION = "Получение игр для клментов";
+        public static final String CATEGORY_TAG_NAME = "Категории игр";
+        public static final String CATEGORY_TAG_DESCRIPTION = "Получение категорий игр";
+    }
+
     public static class Variables {
         public static final int DEFAULT_PAGE_SIZE = 20;
         public static final int DEFAULT_PAGE_NUMBER = 0;
         public static final int TEN_MINUTES = 1000 * 60 * 10;
         public static final int TEN_HOURS = 1000 * 60 * 60 * 10;
         public static final String EMPTY_STRING = "";
+
+        public static final String DEFAULT_USER_MAIL = "user@mail.com";
+        public static final String DEFAULT_USER_PASSWORD = "userPASSWORD123";
+        public static final String DEFAULT_USER_ROLE = "ROLE_CUSTOMER";
+        public static final String DEFAULT_USER_NICKNAME = "userNickname";
+        public static final String DEFAULT_USER_TOKEN = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFt";
+        public static final String DEFAULT_UUID = "70b82203-0f41-47e1-b11c-0fb96f8f9204";
+        public static final String DEFAULT_IMAGE_LINK = "https://bb82cdcce70076216efdbdfb864e275d.jpg";
     }
 
     public static class Message {
