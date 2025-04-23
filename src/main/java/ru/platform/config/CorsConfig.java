0e39e160be78dbe1bfc04029d4061ba6ac6917f4
09NINE90 <razumov.aleksei101@yandex.ru>
2025-04-23 23:44:36 +0500

    cors

--- src/main/java/ru/platform/config/CorsConfig.java
+++ src/main/java/ru/platform/config/CorsConfig.java
@@ -15,9 +15,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(@NotNull CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:5173")
-                        .allowedOrigins("http://85.115.160.5:5173")
-                        .allowedOrigins("http://192.168.0.192:5173")
+                        .allowedOrigins("http://localhost:5173", "http://192.168.0.192:5173", "http://85.115.160.5:5173")
                         .allowedMethods("GET", "POST", "PUT", "DELETE")
                         .allowedHeaders("*")
                         .allowCredentials(true);
