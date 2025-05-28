f314b00f23556726e04bb9e38bbd3f6ff5d5a584
09NINE90 <razumov.aleksei101@yandex.ru>
2025-05-28 16:07:48 +0500

    Расширение маппинга категорий игры

--- src/main/java/ru/platform/games/service/impl/CategoryService.java
+++ src/main/java/ru/platform/games/service/impl/CategoryService.java
@@ -11,6 +11,7 @@ import ru.platform.monitoring.PlatformMonitoring;
 
 import java.util.List;
 import java.util.UUID;
+import java.util.stream.Stream;
 
 @Service
 @RequiredArgsConstructor
@@ -23,14 +24,24 @@ public class CategoryService implements ICategoryService {
     public List<CategoryRsDto> getCategoriesByGameId(String gameId) {
         return repository.findAllByGameId(UUID.fromString(gameId))
                 .stream()
+                .flatMap(this::flattenCategoryWithChildren)
                 .map(this::toCategoryRsDto)
                 .toList();
     }
 
     private CategoryRsDto toCategoryRsDto(CategoryEntity category) {
         return CategoryRsDto.builder()
+                .id(category.getId().toString())
                 .name(category.getName())
                 .build();
+    }
+
+    private Stream<CategoryEntity> flattenCategoryWithChildren(CategoryEntity category) {
+        return Stream.concat(
+                Stream.of(category),
+                category.getSubcategories().stream()
+                        .flatMap(this::flattenCategoryWithChildren)
+        );
     }
 
 }
