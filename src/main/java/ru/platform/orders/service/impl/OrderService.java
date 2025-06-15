1ac9ac1ed0d4096dcc867e777125ac3b9738e8d0
09NINE90 <razumov.aleksei101@yandex.ru>
2025-06-15 14:48:54 +0500

    уровни бустера

--- src/main/java/ru/platform/orders/service/impl/OrderService.java
+++ src/main/java/ru/platform/orders/service/impl/OrderService.java
@@ -27,6 +27,7 @@ import java.util.List;
 import java.util.UUID;
 import java.util.function.Function;
 
+import static ru.platform.LocalConstants.Variables.*;
 import static ru.platform.exception.ErrorType.NOT_FOUND_ERROR;
 import static ru.platform.exception.ErrorType.ORDER_ALREADY_IN_PROGRESS_ERROR;
 import static ru.platform.orders.enumz.OrderStatus.CREATED;
@@ -112,9 +113,40 @@ public class OrderService implements IOrderService {
 
     @Override
     public OrderListRsDto getAllOrders(OrdersByFiltersRqDto request) {
-        request.setStatus(CREATED);
+        UserEntity user = authService.getAuthUser();
+        int userLevel = user.getProfile().getLevel();
+        double ratio = getRatioLevel(userLevel);
+
+        preparationRequest(request, ratio);
         Page<OrderEntity> orders = getServicePageFuncWithSort().apply(request);
-        return mapper.toOrderListRsDto(orders);
+        OrderListRsDto response = mapper.toOrderListRsDto(orders);
+        return recalculationPrice(response, ratio);
+    }
+
+    private void preparationRequest(OrdersByFiltersRqDto request, double ratio) {
+        request.setStatus(CREATED);
+        OrdersByFiltersRqDto.PriceDto requestPrice = request.getPrice();
+        if (requestPrice != null && requestPrice.getPriceFrom() != null && requestPrice.getPriceTo() != null) {
+            requestPrice.setPriceFrom(Math.floor(requestPrice.getPriceFrom() / ratio));
+            requestPrice.setPriceTo(Math.floor(requestPrice.getPriceTo() / ratio));
+        }
+    }
+
+    private OrderListRsDto recalculationPrice(OrderListRsDto response,  double ratio) {
+        List<OrderRsDto> orders =  response.getOrders().stream()
+                .peek(o -> o.setTotalPrice(Math.ceil(o.getTotalPrice() * ratio)))
+                .toList();
+        response.setOrders(orders);
+        return response;
+    }
+
+    private double getRatioLevel(int userLevel) {
+        return switch (userLevel) {
+            case 1 -> BOOSTER_LEVEL_1_PERCENT;
+            case 2 -> BOOSTER_LEVEL_2_PERCENT;
+            case 3 -> BOOSTER_LEVEL_3_PERCENT;
+            default -> throw new IllegalStateException("Unexpected value: " + userLevel);
+        };
     }
 
     @Override
---
+++ src/main/resources/db/migration/V21__add_level_column_user_profile.sql
@@ -0,0 +1,1 @@
+ALTER TABLE user_profile ADD COLUMN level int2 DEFAULT 1;
